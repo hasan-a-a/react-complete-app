c57fc11bb5649b003b57b1517c999aa432fd6022
hasa-a-a <hassan_ali_ahmad@outlook.com>
2024-04-07 21:48:44 +0300

    add featurs and style for header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
 import headerstyles from "../styles/header.module.css"
+import carting from "../images/Cart.png"
 const Header = () => {
     return(
-        <div className={headerstyles.headerContainer}>Header</div>
+        <div className={headerstyles.headerContainer}>
+            <div className={headerstyles.logo}>FreshKart</div>
+            <div className={headerstyles.nav}>
+            <div className={headerstyles.navItem}>Fruits</div>
+            <div className={headerstyles.navItem}>Vegtebales</div>
+            </div>
+            <div className={headerstyles.cart}>
+                <img src={carting} alt=""/>
+                <span>Cart</span>
+            </div>
+            <div className={headerstyles.login}>Login</div>
+            
+        </div>
+        
+
     )
 }
 export default Header;
