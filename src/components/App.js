8092f2b999621260b4df07d6d7044f4918861823
hasa-a-a <hassan_ali_ahmad@outlook.com>
2024-04-08 21:56:10 +0300

    add new components

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
-import {Routes,Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import Items from "./Items";
+import Cart from "./Cart";
+import Final from "./Final";
+import Login from "./Login";
 
 function App() {
   return (
-    <div >
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/" element={<Home />}/>
-      </Routes>
-
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/final" element={<Final />} />
+      <Route path="/items/:type" element={<Items />} />
+    </Routes>
   );
 }
 
